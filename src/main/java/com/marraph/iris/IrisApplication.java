d00eda13d6131777600d0743c1586f942372d35c
Raphael <raphael.goetz.7@gmail.com>
2024-05-01 17:02:48 +0200

    ref: used Java21 language styling

--- src/main/java/com/marraph/iris/IrisApplication.java
+++ src/main/java/com/marraph/iris/IrisApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IrisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IrisApplication.class, args);
+	void main() {
+		SpringApplication.run(IrisApplication.class);
 	}
 
 }
