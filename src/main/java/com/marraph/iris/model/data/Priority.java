2261b992709f0044ecaa7745d1e08a71928be39f
Raphael <raphael.goetz.7@gmail.com>
2024-05-01 17:03:44 +0200

    feat: added data-enums

---
+++ src/main/java/com/marraph/iris/model/data/Priority.java
@@ -0,0 +1,7 @@
+package com.marraph.iris.model.data;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/com/marraph/iris/model/data/Status.java
@@ -0,0 +1,10 @@
+package com.marraph.iris.model.data;
+
+public enum Status {
+    PENDING,
+    PLANING,
+    STARTED,
+    TESTED,
+    FINISHED,
+    ARCHIVED
+}
