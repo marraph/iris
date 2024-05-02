b32e65e5eb4096271747712f8e0de7dbacad005e
Raphael <raphael.goetz.7@gmail.com>
2024-05-02 23:15:39 +0200

    fix: changed int to long inside controller

--- src/main/java/com/marraph/iris/repository/TopicController.java
+++ src/main/java/com/marraph/iris/repository/TopicController.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TopicController extends JpaRepository<Topic, Integer> {
+public interface TopicController extends JpaRepository<Topic, Long> {
 }
