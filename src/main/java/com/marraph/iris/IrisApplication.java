31cfd47a744afff54e8a224feb441cde8b769cd6
Raphael <raphael.goetz.7@gmail.com>
2024-05-05 16:37:21 +0200

    dependencies: rollback to java 21

--- src/main/java/com/marraph/iris/IrisApplication.java
+++ src/main/java/com/marraph/iris/IrisApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IrisApplication {
 
-	void main() {
+	public static void main(String[] args) {
 		SpringApplication.run(IrisApplication.class);
 	}
 
