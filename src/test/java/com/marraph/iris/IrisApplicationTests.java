764de0ab427373d852b814c4a0bc93b6d1be5ca0
Raphael <raphael.goetz.7@gmail.com>
2024-05-19 15:50:39 +0200

    feat: implemented some user-service testing

--- src/test/java/com/marraph/iris/IrisApplicationTests.java
+++ src/test/java/com/marraph/iris/IrisApplicationTests.java
@@ -1,13 +1,27 @@
 package com.marraph.iris;
 
+import com.marraph.iris.service.UserServiceImplTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IrisApplicationTests {
 
+	private final UserServiceImplTest userServiceImplTest;
+
+	public IrisApplicationTests() {
+		this.userServiceImplTest = new UserServiceImplTest();
+	}
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void runAllTests() throws Exception {
+		userServiceImplTest.setUp();
+		userServiceImplTest.testCreate_UserDoesNotExist_ShouldCreateNewUser();
+		userServiceImplTest.testDelete_UserExists_ShouldDeleteUser();
 	}
 
 }
