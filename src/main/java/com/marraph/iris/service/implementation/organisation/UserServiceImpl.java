5841c461db6f358264fae35338e88c6350993df9
Raphael <raphael.goetz.7@gmail.com>
2024-05-20 21:59:28 +0200

    feat: implemented EmailInUseException

--- src/main/java/com/marraph/iris/service/implementation/organisation/UserServiceImpl.java
+++ src/main/java/com/marraph/iris/service/implementation/organisation/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.marraph.iris.service.implementation.organisation;
 
+import com.marraph.iris.exception.EmailInUseException;
 import com.marraph.iris.exception.EntryNotFoundException;
 import com.marraph.iris.model.organisation.User;
 import com.marraph.iris.repository.UserRepository;
@@ -19,6 +20,7 @@ public final class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final ExampleMatcher modelMatcher;
+    private final ExampleMatcher emailMatcher;
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
@@ -28,10 +30,19 @@ public final class UserServiceImpl implements UserService {
                 .withIgnorePaths("id")
                 .withMatcher("username", ignoreCase())
                 .withMatcher("email", ignoreCase());
+
+        this.emailMatcher = ExampleMatcher.matching()
+                .withIgnorePaths("id")
+                .withMatcher("email", ignoreCase());
     }
 
     @Override
     public CompletableFuture<User> create(User entity) {
+
+        this.emailInUse(entity).thenAccept(IsAlreadyInUse -> {
+           if (IsAlreadyInUse) throw new EmailInUseException(entity.getEmail());
+        });
+
         return this.exists(entity).thenCompose(exists -> {
 
             if (!exists) {
@@ -45,6 +56,11 @@ public final class UserServiceImpl implements UserService {
 
     @Override
     public CompletableFuture<User> update(Long id, User updatedEntity) {
+
+        this.emailInUse(updatedEntity).thenAccept(IsAlreadyInUse -> {
+            if (IsAlreadyInUse) throw new EmailInUseException(updatedEntity.getEmail());
+        });
+
         CompletableFuture<User> future = new CompletableFuture<>();
         final var entry = userRepository.findById(id).orElseThrow(() -> new EntryNotFoundException(id));
 
@@ -72,5 +88,10 @@ public final class UserServiceImpl implements UserService {
     @Override
     public CompletableFuture<Boolean> exists(User entity) {
         return CompletableFuture.completedFuture(userRepository.exists(Example.of(entity, modelMatcher)));
+    }
+
+    @Override
+    public CompletableFuture<Boolean> emailInUse(User entity) {
+        return CompletableFuture.completedFuture(userRepository.exists(Example.of(entity, emailMatcher)));
     }
 }
