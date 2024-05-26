27b5f4dff9ff2f4260e7c0d92a81fa1445a41282
Raphael <raphael.goetz.7@gmail.com>
2024-05-26 12:11:16 +0200

    fix: made EmailInUseAdvice final

--- src/main/java/com/marraph/iris/advice/EmailInUseAdvice.java
+++ src/main/java/com/marraph/iris/advice/EmailInUseAdvice.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class EmailInUseAdvice {
+public final class EmailInUseAdvice {
 
 
     @ResponseBody
