c6a4d2759a1a5540db978801927227098a29af2c
Raphael <raphael.goetz.7@gmail.com>
2024-05-28 20:24:42 +0200

    feat: added column fields

--- src/main/java/com/marraph/iris/model/time/Absence.java
+++ src/main/java/com/marraph/iris/model/time/Absence.java
@@ -2,6 +2,7 @@ package com.marraph.iris.model.time;
 
 import com.marraph.iris.model.Auditable;
 import com.marraph.iris.model.data.AbsenceType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -15,9 +16,15 @@ import java.util.Date;
 @Table(name = "Absences")
 public final class Absence extends Auditable {
 
+    @Column(nullable = false)
     Date startDate;
+
+    @Column(nullable = false)
     Date endDate;
+
+    @Column(nullable = false)
     AbsenceType absenceType;
+
     String comment;
 
 }
