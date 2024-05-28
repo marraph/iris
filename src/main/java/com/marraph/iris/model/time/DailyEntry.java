7363456665c9a10cf4e9f9bcec98371ad3eb2843
Raphael <raphael.goetz.7@gmail.com>
2024-05-28 20:24:07 +0200

    fix: added missing attributes and removed unused ones

--- src/main/java/com/marraph/iris/model/time/DailyEntry.java
+++ src/main/java/com/marraph/iris/model/time/DailyEntry.java
@@ -3,13 +3,11 @@ package com.marraph.iris.model.time;
 import com.marraph.iris.model.Auditable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
-import java.util.List;
 
 @Getter
 @Setter
@@ -17,13 +15,12 @@ import java.util.List;
 @Table(name = "DailyEntries")
 public final class DailyEntry extends Auditable {
 
-    @OneToMany
-    private List<TimeEntry> entry;
-
     @Column(nullable = false)
     private Date startDate;
 
     @Column(nullable = false)
     private Date endDate;
+
+    private String comment;
 
 }
