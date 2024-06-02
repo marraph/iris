b417a50ea1942a24182500b6cdba33216e31cd80
Raphael <raphael.goetz.7@gmail.com>
2024-06-02 14:23:10 +0200

    fix: rollback on tests

--- src/test/java/com/marraph/iris/IrisApplicationTests.java
+++ src/test/java/com/marraph/iris/IrisApplicationTests.java
@@ -1,17 +1,10 @@
 package com.marraph.iris;
 
-import com.marraph.iris.service.UserServiceImplTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IrisApplicationTests {
-
-	private final UserServiceImplTest userServiceImplTest;
-
-	public IrisApplicationTests() {
-		this.userServiceImplTest = new UserServiceImplTest();
-	}
 
 	@Test
 	void contextLoads() {
@@ -19,9 +12,6 @@ class IrisApplicationTests {
 
 	@Test
 	void runAllTests() throws Exception {
-		userServiceImplTest.setUp();
-		userServiceImplTest.testCreate_UserDoesNotExist_ShouldCreateNewUser();
-		userServiceImplTest.testDelete_UserExists_ShouldDeleteUser();
 	}
 
 }
