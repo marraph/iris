51acfe913cacbe71c59ca95ff1409d419bd66d91
Raphael <raphael.goetz.7@gmail.com>
2024-06-02 14:22:59 +0200

    fix: added wildcard to CORS

--- src/main/java/com/marraph/iris/controller/AbstractController.java
+++ src/main/java/com/marraph/iris/controller/AbstractController.java
@@ -14,27 +14,32 @@ public abstract class AbstractController<T> {
         this.service = service;
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/create")
     public CompletableFuture<ResponseEntity<T>> createEntity(@RequestBody T entity) {
         return service.create(entity).thenApply(ResponseEntity::ok);
     }
 
+    @CrossOrigin(origins = "*")
     @PutMapping("/update/{id}")
     public CompletableFuture<ResponseEntity<T>> updateEntity(@PathVariable Long id, @RequestBody T entity) {
         return service.update(id, entity).thenApply(ResponseEntity::ok);
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/get/{id}")
     public CompletableFuture<ResponseEntity<T>> getEntityById(@PathVariable Long id) {
         return service.getById(id)
                 .thenApply(opt -> opt.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build()));
     }
 
+    @CrossOrigin(origins = "*")
     @DeleteMapping("/delete/{id}")
     public void deleteEntity(@PathVariable Long id) {
         service.delete(id);
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/exists")
     public CompletableFuture<ResponseEntity<Boolean>> entityExists(@RequestBody T entity) {
         return service.exists(entity).thenApply(ResponseEntity::ok);
