df9e930d57e868bbb27bc6019e95afe8f798574c
Raphael <raphael.goetz.7@gmail.com>
2024-06-24 10:28:03 +0200

    feat: added connection entry exception

---
+++ src/main/java/com/marraph/iris/exception/ConnectEntryException.java
@@ -0,0 +1,9 @@
+package com.marraph.iris.exception;
+
+public class ConnectEntryException extends RuntimeException {
+
+    public ConnectEntryException(String message) {
+        super(message);
+    }
+
+}
